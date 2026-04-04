30e1c8d99f2e6a6327caab626e783572ae076af2
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-04-04 22:42:04 +0545

    theme dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: bgColor),
       home: const Text('Flutter Demo Home Page'),
     );
   }
